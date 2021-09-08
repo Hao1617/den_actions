3295f68453a7f12e02ad792b4730b7e0687f0f61
li15838287467 <1617614731@qq.com>
2021-09-08 11:17:48 +0800

    初始化定时器

--- index.js
+++ index.js
@@ -8,14 +8,15 @@ let score = 0;
 
 const headers = {
   'content-type': 'application/json; charset=utf-8',
-  'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36',
+  'user-agent':
+    'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36',
   'accept-encoding': 'gzip, deflate, br',
   'accept-language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
   'sec-ch-ua': '"Chromium";v="88", "Google Chrome";v="88", ";Not A Brand";v="99"',
   'sec-ch-ua-mobile': '?0',
   referer: 'https://juejin.cn/',
   accept: '*/*',
-  cookie
+  cookie,
 };
 
 // 抽奖
@@ -24,8 +25,8 @@ const drawFn = async () => {
   const today = await fetch('https://api.juejin.cn/growth_api/v1/lottery_config/get', {
     headers,
     method: 'GET',
-    credentials: 'include'
-  }).then((res) => res.json());
+    credentials: 'include',
+  }).then(res => res.json());
 
   if (today.err_no !== 0) return Promise.reject('已经签到！免费抽奖失败！');
   if (today.data.free_count === 0) return Promise.resolve('签到成功！今日已经免费抽奖！');
@@ -34,8 +35,8 @@ const drawFn = async () => {
   const draw = await fetch('https://api.juejin.cn/growth_api/v1/lottery/draw', {
     headers,
     method: 'POST',
-    credentials: 'include'
-  }).then((res) => res.json());
+    credentials: 'include',
+  }).then(res => res.json());
 
   if (draw.err_no !== 0) return Promise.reject('已经签到！免费抽奖异常！');
   console.log(JSON.stringify(draw, null, 2));
@@ -49,8 +50,8 @@ const drawFn = async () => {
   const today_status = await fetch('https://api.juejin.cn/growth_api/v1/get_today_status', {
     headers,
     method: 'GET',
-    credentials: 'include'
-  }).then((res) => res.json());
+    credentials: 'include',
+  }).then(res => res.json());
 
   if (today_status.err_no !== 0) return Promise.reject('签到失败！');
   if (today_status.data) return Promise.resolve('今日已经签到！');
@@ -59,26 +60,26 @@ const drawFn = async () => {
   const check_in = await fetch('https://api.juejin.cn/growth_api/v1/check_in', {
     headers,
     method: 'POST',
-    credentials: 'include'
-  }).then((res) => res.json());
+    credentials: 'include',
+  }).then(res => res.json());
 
   if (check_in.err_no !== 0) return Promise.reject('签到异常！');
   return Promise.resolve(`签到成功！当前积分；${check_in.data.sum_point}`);
 })()
-  .then((msg) => {
+  .then(msg => {
     console.log(msg);
     return fetch('https://api.juejin.cn/growth_api/v1/get_cur_point', {
       headers,
       method: 'GET',
-      credentials: 'include'
-    }).then((res) => res.json());
+      credentials: 'include',
+    }).then(res => res.json());
   })
-  .then((res) => {
+  .then(res => {
     console.log(res);
     score = res.data;
     return drawFn();
   })
-  .then((msg) => {
+  .then(msg => {
     console.log(msg);
     return sendMail({
       from: '掘金',
@@ -88,13 +89,13 @@ const drawFn = async () => {
         <h1 style="text-align: center">自动签到通知</h1>
         <p style="text-indent: 2em">签到结果：${msg}</p>
         <p style="text-indent: 2em">当前积分：${score}</p><br/>
-      `
+      `,
     }).catch(console.error);
   })
   .then(() => {
     console.log('邮件发送成功！');
   })
-  .catch((err) => {
+  .catch(err => {
     sendMail({
       from: '掘金',
       to,
@@ -103,6 +104,6 @@ const drawFn = async () => {
         <h1 style="text-align: center">自动签到通知</h1>
         <p style="text-indent: 2em">执行结果：${err}</p>
         <p style="text-indent: 2em">当前积分：${score}</p><br/>
-      `
+      `,
     }).catch(console.error);
   });
